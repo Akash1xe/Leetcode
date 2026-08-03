27665eb692d65c310c30c3f191e7f665df40750f
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-03 13:45:51 +0530

    Time: 395 ms (26.56%) | Memory: 19.4 MB (53.65%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,39 +1,65 @@
 class Solution {
 public:
     vector<int> countOfPairs(int n, int x, int y) {
-        vector<vector<int>> grid(n+1, vector<int>(n+1, 100000));
-        
-        for(int j = 1; j < n; j++) {
-            grid[j][j+1] = 1;
-            grid[j+1][j] = 1;
+
+        // grid[i][j] = shortest distance from house i to house j
+        // Initialize with a very large value (acts as INF)
+        vector<vector<int>> grid(n + 1, vector<int>(n + 1, 1000));
+
+        // Distance from a house to itself is 0
+        for (int i = 1; i <= n; i++) {
+            grid[i][i] = 0;
         }
+
+        // Build the line graph:
+        // 1-2-3-...-n
+        for (int j = 1; j < n; j++) {
+            grid[j][j + 1] = 1;
+            grid[j + 1][j] = 1;
+        }
+
+        // Add the extra bidirectional road between x and y
         grid[x][y] = 1;
         grid[y][x] = 1;
-        
-        for(int via = 1; via <= n; via++) {
-            
-            for(int i = 1; i <= n; i++) {
-                for(int j = 1; j <= n; j++) {
-                    
-                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
-                    
+
+        // Floyd-Warshall Algorithm
+        // Try every node as an intermediate (via) node
+        for (int via = 1; via <= n; via++) {
+
+            // Source node
+            for (int i = 1; i <= n; i++) {
+
+                // Destination node
+                for (int j = 1; j <= n; j++) {
+
+                    // If travelling through 'via' gives a shorter path,
+                    // update the shortest distance.
+                    grid[i][j] = min(grid[i][j],
+                                     grid[i][via] + grid[via][j]);
                 }
             }
         }
-        
+
+        // result[d - 1] = number of ordered pairs
+        // having shortest distance exactly d
         vector<int> result(n);
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
 
-                if(i != j) {
+        // Check every ordered pair of houses
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // Ignore same house
+                if (i != j) {
+
+                    // Shortest distance between i and j
                     int val = grid[i][j];
-                    result[val-1]++;
-                }
 
+                    // Increase the count for this distance
+                    result[val - 1]++;
+                }
             }
         }
-        
+
         return result;
-        
     }
 };
