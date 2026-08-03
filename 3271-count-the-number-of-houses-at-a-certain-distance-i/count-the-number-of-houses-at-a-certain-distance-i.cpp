9dff401acb6f26db215311ed5f9087e4e4522307
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-03 12:27:16 +0530

    Time: 399 ms (24.31%) | Memory: 19.2 MB (62.15%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     vector<int> countOfPairs(int n, int x, int y) {
-
-        // answer[i] = number of ordered pairs having distance (i + 1)
-        vector<int> ans(n, 0);
-
-        // Try every ordered pair (i, j)
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= n; j++) {
-
-                // Ignore same house
-                if (i == j)
-                    continue;
-
-                // Option 1: Walk directly on the line
-                int direct = abs(i - j);
-
-                // Option 2: Use extra road x -> y
-                int path1 = abs(i - x) + 1 + abs(j - y);
-
-                // Option 3: Use extra road y -> x
-                int path2 = abs(i - y) + 1 + abs(j - x);
-
-                // Shortest possible distance
-                int dist = min({direct, path1, path2});
-
-                // Store frequency
-                ans[dist - 1]++;
+        vector<vector<int>> grid(n+1, vector<int>(n+1, 100000));
+        
+        for(int j = 1; j < n; j++) {
+            grid[j][j+1] = 1;
+            grid[j+1][j] = 1;
+        }
+        grid[x][y] = 1;
+        grid[y][x] = 1;
+        
+        for(int via = 1; via <= n; via++) {
+            
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= n; j++) {
+                    
+                   grid[i][j] = min(grid[i][j], grid[i][via] + grid[via][j]);
+                    
+                }
             }
         }
+        
+        vector<int> result(n);
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
 
-        return ans;
+                if(i != j) {
+                    int val = grid[i][j];
+                    result[val-1]++;
+                }
+
+            }
+        }
+        
+        return result;
+        
     }
 };
