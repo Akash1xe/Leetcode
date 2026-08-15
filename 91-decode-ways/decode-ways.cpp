715dcc5246c6dea30a5601dfde00b48613969a30
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-16 00:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (46.57%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,48 +1,27 @@
-/*
-    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=HW-y3gvQTVQ
-    Company Tags                : Facebook, Uber, Google, Facebook, Microsoft
-    Leetcode Link               : https://leetcode.com/problems/decode-ways/
-    Four approaches             : Memoized, Top Down, Better Top Down, O(1) space DP
-*/
-
-/************************************************************************ C++ ***************************************************************/
-//Approach-1 (Using Recursion + Memoization)
-//T.C : O(n) after memoization (without memoization - O(2^n)
-//S.C : O(101) ~= O(1)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
-    int t[101];
-    int solve(int i, string &s, int &n) {
-        if(t[i] != -1) {
-            return t[i];
-        }
-
-        if(i == n) {
-            return t[i] = 1; //one valid split done
-        }
-
-        if(s[i] == '0') {
-            return t[i] = 0; //not possible to split
-        }
-
-        int result     = solve(i+1, s, n);
+    int bottom_up_2(string&s, int n) {
+        vector<int> t(n+1, 0);
+        //t[i] = ways to decode string of length i
+        t[0] = 1;
+        t[1] = s[0]=='0'?0:1;
         
-        if(i+1 < n) {
-            if(s[i] == '1' || (s[i] == '2' && s[i+1] <= '6'))
-                result += solve(i+2, s, n);
+        for(int i = 2; i<=n; i++) {            
+            if(s[i-1] != '0')
+                t[i] += t[i-1];
+                
+            if(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] < '7'))
+                t[i] += t[i-2];
+            
         }
-
-        
-        return t[i] = result;
-
+        return t[n];
     }
-
-    int numDecodings(string s) {
+    
+    int numDecodings(string &s) {
         int n = s.length();
-        
-        memset(t, -1, sizeof(t));
-        return solve(0, s, n);
-        
-
+        vector<int> t(n+1, -1);
+        return bottom_up_2(s, n);
     }
 };
