bfb244b37100e959f6eaf03c177dce31536a883f
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-20 12:22:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (64.32%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,31 +1,50 @@
+// Approach-2 (Bottom Up Way-1)
+// T.C : O(n)
+// S.C : O(n)
+
 class Solution {
 public:
-    int numDecodings(string &s) {
-        int n = s.length();
+    int bottom_up_1(string &s, int n) {
 
-        // dp[i] = number of ways to decode the first i characters
+        // dp[i] = number of ways to decode
+        // the substring starting from index i
         vector<int> dp(n + 1, 0);
 
-        // Empty string has 1 valid way
-        dp[0] = 1;
+        // Base case:
+        // If we reach the end of the string,
+        // there is exactly 1 way to decode it.
+        dp[n] = 1;
 
-        // First character cannot be '0'
-        dp[1] = (s[0] == '0') ? 0 : 1;
+        // Traverse from right to left
+        for (int i = n - 1; i >= 0; i--) {
 
-        for (int i = 2; i <= n; i++) {
-
-            // Take current character as a single digit
-            if (s[i - 1] != '0') {
-                dp[i] += dp[i - 1];
+            // '0' cannot be decoded by itself
+            if (s[i] == '0') {
+                dp[i] = 0;
             }
+            else {
 
-            // Take current + previous character as a two-digit number
-            if (s[i - 2] == '1' ||
-                (s[i - 2] == '2' && s[i - 1] < '7')) {
-                dp[i] += dp[i - 2];
+                // Take s[i] as a single digit
+                dp[i] = dp[i + 1];
+
+                // Take s[i] and s[i+1] together as a
+                // valid two-digit number
+                if (i < n - 1 &&
+                    (s[i] == '1' ||
+                    (s[i] == '2' && s[i + 1] < '7'))) {
+
+                    dp[i] += dp[i + 2];
+                }
             }
         }
 
-        return dp[n];
+        // dp[0] = number of ways to decode the entire string
+        return dp[0];
+    }
+
+    int numDecodings(string &s) {
+        int n = s.length();
+
+        return bottom_up_1(s, n);
     }
 };
