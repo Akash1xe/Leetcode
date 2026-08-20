83e15a44ee3cf7404e35d78d0056ecdd1891130d
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-20 12:10:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.25%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,27 +1,49 @@
-//T.C : O(n)
-//S.C : O(n)
 class Solution {
 public:
-    int bottom_up_2(string&s, int n) {
-        vector<int> t(n+1, 0);
-        //t[i] = ways to decode string of length i
-        t[0] = 1;
-        t[1] = s[0]=='0'?0:1;
-        
-        for(int i = 2; i<=n; i++) {            
-            if(s[i-1] != '0')
-                t[i] += t[i-1];
-                
-            if(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] < '7'))
-                t[i] += t[i-2];
-            
+    int t[101];
+
+    // Number of ways to decode string starting from index i
+    int solve(int i, string &s, int &n) {
+
+        // Already calculated
+        if(t[i] != -1) {
+            return t[i];
         }
-        return t[n];
+
+        // Reached end successfully
+        if(i == n) {
+            return t[i] = 1;
+        }
+
+        // 0 cannot be decoded alone
+        if(s[i] == '0') {
+            return t[i] = 0;
+        }
+
+        // Choice 1: take one digit
+        int result = solve(i + 1, s, n);
+
+        // Choice 2: take two digits
+        if(i + 1 < n) {
+
+            // Valid two-digit number:
+            // 10-19 OR 20-26
+            if(s[i] == '1' ||
+              (s[i] == '2' && s[i + 1] <= '6')) {
+
+                result += solve(i + 2, s, n);
+            }
+        }
+
+        return t[i] = result;
     }
-    
-    int numDecodings(string &s) {
+
+    int numDecodings(string s) {
+
         int n = s.length();
-        vector<int> t(n+1, -1);
-        return bottom_up_2(s, n);
+
+        memset(t, -1, sizeof(t));
+
+        return solve(0, s, n);
     }
 };
