b0216f3c252c29afd1e3a9d0e67057813f6ff12b
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-21 09:44:09 +0530

    Time: 1 ms (27.45%) | Memory: 8.9 MB (67.29%) - LeetSync

--- 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
+++ 806-domino-and-tromino-tiling/domino-and-tromino-tiling.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
 
-// code with mik 
-    int numTilings(int n) {
+    int solve(int n, vector<int>& dp) {
 
-        const int M = 1000000007;
-
-        vector<int> dp(n + 1, -1);
-
-        // Base cases
         if (n == 1 || n == 2)
             return n;
 
         if (n == 3)
             return 5;
 
-        // dp[n] = 2 * dp[n-1] + dp[n-3]
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 5;
+        if (dp[n] != -1)
+            return dp[n];
 
-        for (int i = 4; i <= n; i++) {
-            dp[i] = (2LL * dp[i - 1] + dp[i - 3]) % M;
-        }
+        const int M = 1000000007;
 
-        return dp[n];
+        return dp[n] =
+            (2LL * solve(n - 1, dp) + solve(n - 3, dp)) % M;
+    }
+
+    int numTilings(int n) {
+
+        vector<int> dp(n + 1, -1);
+
+        return solve(n, dp);
     }
 };
