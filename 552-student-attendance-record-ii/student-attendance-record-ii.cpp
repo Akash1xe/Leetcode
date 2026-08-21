6e43bc0a0c90758581770018b2b2889c57e98658
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-08-21 12:31:35 +0530

    Time: 23 ms (93.53%) | Memory: 10.3 MB (84.87%) - LeetSync

--- 552-student-attendance-record-ii/student-attendance-record-ii.cpp
+++ 552-student-attendance-record-ii/student-attendance-record-ii.cpp
@@ -1,34 +1,36 @@
+//Approach-2 (Using Bottom Up - Xerox of Recursion from Approach-1)
+//T.C : O(n)
+//S.C : O(n)
 class Solution {
 public:
-    int M = 1e9 + 7;
-
-    int solve(int n, int absent, int consecutive_late,
-              vector<vector<vector<int>>>& dp) {
-
-        if (absent >= 2 || consecutive_late >= 3)
-            return 0;
-
-        if (n == 0)
-            return 1;
-
-        if (dp[n][absent][consecutive_late] != -1)
-            return dp[n][absent][consecutive_late];
-
-        int A = solve(n - 1, absent + 1, 0, dp) % M;
-        int L = solve(n - 1, absent, consecutive_late + 1, dp) % M;
-        int P = solve(n - 1, absent, 0, dp) % M;
-
-        return dp[n][absent][consecutive_late] =
-            ((A + L) % M + P) % M;
-    }
+    int M = 1e9+7;
 
     int checkRecord(int n) {
+        int t[100001][2][3]; 
 
-        vector<vector<vector<int>>> dp(
-            n + 1,
-            vector<vector<int>>(2, vector<int>(3, -1))
-        );
+        //t[i][A][L] = # award possibilities till ith day , having j absence, k consecutive lates
 
-        return solve(n, 0, 0, dp);
+        //Base Case - 0 number of days. So, t[0][A][L], we return 1
+        for (int A = 0; A <= 1; ++A) {
+            for (int L = 0; L <= 2; ++L) {
+                t[0][A][L] = 1;
+            }
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int A = 0; A <= 1; A++) {
+                for(int L = 0; L <= 2; L++) {
+
+                    long long result = t[i-1][A][0];         // P ---> solve(n-1, absent, 0) % M;
+                    result += (L < 2 ? t[i-1][A][L+1] : 0);  // L ---> solve(n-1, absent, consecutive_late+1) % M;
+                    result += (A == 0 ? t[i-1][A+1][0]: 0);  // A ---> solve(n-1, absent+1, 0) % M;
+                    
+                    t[i][A][L] = result % M;
+                }
+            }
+        }
+
+        return t[n][0][0]; //---> return solve(n, 0, 0);
+
     }
 };
