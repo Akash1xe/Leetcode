344f2257ed7e1c8de850f1c69efa219a5acb5abb
Akash1xe <121711965+Akash1xe@users.noreply.github.com>
2026-09-26 04:17:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (86.59%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,50 +1,46 @@
-// Approach-2 (Bottom Up Way-1)
-// T.C : O(n)
-// S.C : O(n)
+/*
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=HW-y3gvQTVQ
+    Company Tags                : Facebook, Uber, Google, Facebook, Microsoft
+    Leetcode Link               : https://leetcode.com/problems/decode-ways/
+    Four approaches             : Memoized, Top Down, Better Top Down, O(1)
+   space DP
+*/
 
+/************************************************************************ C++
+ * ***************************************************************/
+// Approach-1 (Using Recursion + Memoization)
+// T.C : O(n) after memoization (without memoization - O(2^n)
+// S.C : O(101) ~= O(1)
 class Solution {
 public:
-    int bottom_up_1(string &s, int n) {
-
-        // dp[i] = number of ways to decode
-        // the substring starting from index i
-        vector<int> dp(n + 1, 0);
-
-        // Base case:
-        // If we reach the end of the string,
-        // there is exactly 1 way to decode it.
-        dp[n] = 1;
-
-        // Traverse from right to left
-        for (int i = n - 1; i >= 0; i--) {
-
-            // '0' cannot be decoded by itself
-            if (s[i] == '0') {
-                dp[i] = 0;
-            }
-            else {
-
-                // Take s[i] as a single digit
-                dp[i] = dp[i + 1];
-
-                // Take s[i] and s[i+1] together as a
-                // valid two-digit number
-                if (i < n - 1 &&
-                    (s[i] == '1' ||
-                    (s[i] == '2' && s[i + 1] < '7'))) {
-
-                    dp[i] += dp[i + 2];
-                }
-            }
+    int t[101];
+    int solve(int i, string& s, int& n) {
+        if (t[i] != -1) {
+            return t[i];
         }
 
-        // dp[0] = number of ways to decode the entire string
-        return dp[0];
+        if (i == n) {
+            return t[i] = 1; // one valid split done
+        }
+
+        if (s[i] == '0') {
+            return t[i] = 0; // not possible to split
+        }
+
+        int result = solve(i + 1, s, n);
+         int result2 =0;
+        if (i + 1 < n) {
+            if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6'))
+                result2 = solve(i + 2, s, n);
+        }
+
+        return t[i] = result + result2;
     }
 
-    int numDecodings(string &s) {
+    int numDecodings(string s) {
         int n = s.length();
 
-        return bottom_up_1(s, n);
+        memset(t, -1, sizeof(t));
+        return solve(0, s, n);
     }
 };
